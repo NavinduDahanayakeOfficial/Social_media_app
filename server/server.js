18f91e39d7b23831d1398ecaafbfcc24c29ffabf
Navindu <dahanayakenw.20@uom.lk>
2024-01-01 06:38:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,44 +1,44 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import multer from 'multer';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { register } from './controllers/auth.js';
-import { verifyToken } from './middleware/auth.js';
-import { createPost } from './controllers/posts.js';
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
 
 //* CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); //get the directory of the current file/the module 
+const __dirname = path.dirname(__filename); //get the directory of the current file/the module
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(morgan('common'));
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //set the directory of assets / where we keep our images
 //we store our images in locally
 
 //*FILE STORAGE
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/assets");
-    },
-    filename: (req, file, cb) => {
-        cd(null, file.originalname)
-    }
-})
-const upload = multer({storage});
+   destination: (req, file, cb) => {
+      cb(null, "public/assets");
+   },
+   filename: (req, file, cb) => {
+      cd(null, file.originalname);
+   },
+});
+const upload = multer({ storage });
 
 //* Routes with files
 app.post("/auth/register", upload.single("picture"), register);
@@ -47,20 +47,20 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 //* Routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes)
+app.use("/posts", postRoutes);
 
 // * Mongoose setup
 const PORT = process.env.PORT || 6000;
 mongoose
-    .connect(process.env.MONGO_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        })
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
+   .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => {
+      app.listen(PORT, () => {
+         console.log(`Server is running on port ${PORT}`);
+      });
+   })
+   .catch((error) => {y
+      console.log(error);
+   });
